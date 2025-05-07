d95f2fc3d402230f73f5adaadf07d77ab76d7cc9
StillMix <stillmixplaygames@gmail.com>
2025-05-07 14:00:50 +0300

    new landing

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'devextreme/dist/css/dx.material.blue.light.css';
-import 'devextreme/dist/js/dx.all.js'
-import { DxButton, DxTextBox, DxDateBox, DxTextArea } from 'devextreme-vue'
-import '@/assets/style/global.scss'
+import "./assets/styles/index.scss";
 
 const app = createApp(App)
-
-app.component('DxButton', DxButton)
-app.component('DxTextBox', DxTextBox)
-app.component('DxDateBox', DxDateBox)
-app.component('DxTextArea', DxTextArea)
 
 app.mount('#app')
